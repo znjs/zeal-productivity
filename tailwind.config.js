0200681c2e40f0fc9cb73acd3b17683651f4643f
znjs <jasrikar@gmail.com>
2022-04-05 13:45:40 +0530

    config: added packages and env file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backdropBlur: {
+        xs: "2px",
+      },
+      minHeight: {
+        xl: "10rem",
+      },
+      maxHeight: {
+        xl: "70vh",
+      },
+      outline: {
+        unset: "unset",
+      },
+    },
   },
   plugins: [],
 };
